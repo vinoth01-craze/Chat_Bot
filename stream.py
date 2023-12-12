d812beda7e1c4992b20ea23ed88dfdeea5c692aa
vinoth01-craze <vinothsangari6725@gmail.com>
2023-12-12 21:25:40 +0500

    from

--- stream.py
+++ stream.py
@@ -3,9 +3,15 @@ import streamlit as st
 def simple_chatbot(user_input):
     user_input = user_input.lower()
     if 'my name' in user_input:
-        return 'your name is VARUNA)'
+        return 'your name is VARUNA'
+    elif 'my mother' in user_input:
+        return 'your mother name is KAMACHI.\n I know she is very HARD WORKING women.\n She look very BEAUTIFUL'
+    elif 'father' in user_input:
+        return 'your father name is SUGUMAR\n He is a ORDINARY PERSON.'
     elif 'brother' in user_input:
         return ('your brother name is DIVID ...\n its amazing it is a palindrom letter right...')
+    elif 'divid' in user_input:
+        return ('Divid best friend is ISHWARYA..\n she is also cute , But not like DIVID.because divid is a handsome guy')
     else:
         return 'I am a simple chatbot and can respond to basic greetings.'
 

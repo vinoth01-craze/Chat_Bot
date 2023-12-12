c1fe46a9f7679bd964ef89a167a4244f83e0bb2b
vinoth01-craze <vinothsangari6725@gmail.com>
2023-12-12 15:28:08 +0500

    initial

---
+++ stream.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def simple_chatbot(user_input):
+    user_input = user_input.lower()
+    if 'name' in  user_input:
+        return 'your name is VINOTH'
+    elif 'hello' in user_input:
+        return 'hello i am chatbot how can i help you'
+    elif 'age' in user_input:
+        return 'your age is 19..'
+    elif 'college' in user_input:
+        return 'you are currently doing in NANDHA ENGINEERING COLLEGE'
+    elif 'bye' in user_input:
+        return 'bye have a nice day to you..'
+    else:
+        return 'I am a simple chatbot and can respond to basic greetings.'
+
+
+st.title("Simple Chatbot with Streamlit")
+
+user_input = st.text_input("You: ", "")
+if user_input:
+    response = simple_chatbot(user_input)
+    st.text_area("Bot:", value=response, height=100)

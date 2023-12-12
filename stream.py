318f36790108f095bbb17d9f1261b15a4e89d230
vinoth01-craze <vinothsangari6725@gmail.com>
2023-12-12 19:29:26 +0500

    from

--- stream.py
+++ stream.py
@@ -1,25 +1,44 @@
 import streamlit as st
-
-def simple_chatbot(user_input):
-    user_input = user_input.lower()
-    if 'name' in  user_input:
-        return 'your name is VINOTH'
-    elif 'hello' in user_input:
-        return 'hello i am chatbot how can i help you'
-    elif 'age' in user_input:
-        return 'your age is 19..'
-    elif 'college' in user_input:
-        return 'you are currently doing in NANDHA ENGINEERING COLLEGE'
-    elif 'bye' in user_input:
-        return 'bye have a nice day to you..'
-    else:
-        return 'I am a simple chatbot and can respond to basic greetings.'
+from datetime import datetime, timedelta
 
 
-st.title("Simple Chatbot with Streamlit")
+# Function to display a reminder for the next period
+def display_reminder(current_day, current_time, timetable):
+    # Logic to find the next period
+    # You need to adjust this based on your timetable structure
+    next_period = find_next_period(current_day, current_time, timetable)
 
-user_input = st.text_input("You: ", "")
-if user_input:
-    response = simple_chatbot(user_input)
-    if st.button("Send "):
-        st.text_area("Bot:", value=response, height=100)
+    # Display reminder message
+    if next_period:
+        st.info(f"Next period: {next_period} will start soon. Be prepared!")
+
+
+# Function to find the next period based on current day and time
+def find_next_period(current_day, current_time, timetable):
+
+ def main():
+    st.title("College Timetable App")
+
+    # Get user input for the number of subjects
+    num_subjects = st.number_input("Enter the number of subjects:", min_value=1, value=5)
+
+    # Initialize timetable
+    timetable = {day: {} for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]}
+
+    # Input subjects and hours for each day
+    for day in timetable:
+        st.header(f"{day}'s timetable")
+        for i in range(1, num_subjects + 1):
+            subject_name = st.text_input(f"Subject {i}:", f"Subject {i}")
+            hours = st.number_input(f"Hours for {subject_name} on {day}:", min_value=1, value=1)
+
+            timetable[day][subject_name] = hours
+
+    # Get current day and time
+    current_day = datetime.now().strftime("%A")
+    current_time = st.time_input("Current time:", value=datetime.now().time())
+
+    # Display reminder for the next period
+    display_reminder(current_day, current_time, timetable)
+
+
